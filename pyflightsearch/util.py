9d0041398b753d1a00227101efda1deed4839c84
Josh Imbriani <joshimbriani@gmail.com>
2019-06-26 20:47:10 -0700

    Finished dates meeting conditions

--- pyflightsearch/util.py
+++ pyflightsearch/util.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
 
 from pyflightsearch.constants import API_BASE
+from pyflightsearch.errors.configuration_error import ConfigurationError
 
 def build_flight_search_queries(flight_search_builder):
     queries = []
@@ -8,9 +9,26 @@ def build_flight_search_queries(flight_search_builder):
     return API_BASE
 
 def generate_dates_meeting_conditions(start_date, departure_day, return_day, lookahead_weeks):
-    start_end_pair = []
     if not isinstance(start_date, date):
-        pass
+        return ConfigurationError("start_date input to generate_dates_meeting_conditions must be a date object")
+    if type(departure_day) != int:
+        return ConfigurationError("departure_day input to generate_dates_meeting_conditions must be a int")
+    if type(return_day) != int:
+        return ConfigurationError("return_day input to generate_dates_meeting_conditions must be a int")
+    if type(lookahead_weeks) != int:
+        return ConfigurationError("lookahead_weeks input to generate_dates_meeting_conditions must be a int")
+
+    pairs = []
+    start = None
+    for day in daterange(start_date, lookahead_weeks * 7 + 6):
+        if day.weekday() == departure_day:
+            start = day
+
+        if start and day != start and day.weekday() == return_day:
+            pairs.append((start, day))
+            start = None
+
+    return pairs
 
 def daterange(start_date, duration):
     for i in range(duration):
