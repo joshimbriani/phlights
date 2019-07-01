fb241937344a067bebf0a84de0a627f119be5e57
Josh Imbriani <joshimbriani@gmail.com>
2019-06-30 21:06:34 -0700

    Adding tmp to ignore

--- tmp.py
+++
@@ -1,9 +0,0 @@
-from phlights.flightsearch import FlightSearch
-from phlights.util import build_flight_search_queries
-from datetime import date
-def test():
-    for trip in FlightSearch.weekend().from_place("SFO").to_place("MCO").start_from(date(year=2019, month=8, day=1)).search():
-        print(trip)
-
-if __name__ == "__main__":
-    test()
